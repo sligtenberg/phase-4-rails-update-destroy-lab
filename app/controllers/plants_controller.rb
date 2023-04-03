c829e7825fa797ae48dbb67476d78cd790c8fd60
stephen ligtenberg <bountyhunter24@gmail.com>
2023-04-03 15:42:41 -0600

    commit tree bd7f2afad4a5fc0569f38df265e44d7c04e42ea5

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,16 +6,30 @@ class PlantsController < ApplicationController
     render json: plants
   end
 
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
-
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # GET /plants/:id
+  def show
+    plant = find_plant
+    render json: plant
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
@@ -23,4 +37,9 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
 end
